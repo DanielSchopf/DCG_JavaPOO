ae44f8a24a680accdc723b022bbf205a421bbaba
Daniel Schopf <74994656+DanielSchopf@users.noreply.github.com>
2023-04-04 20:55:15 -0300

    Create Conta.java

---
+++ Aula_3/Exercicio01/Conta.java
@@ -0,0 +1,33 @@
+package Aula3;
+public class Conta {
+    private double saldo;
+
+    public String titular;
+    public void saque(double valor){
+        if (valor > this.saldo) {
+            System.out.println("Saldo insuficiente! ");
+        } else{
+            this.saldo = this.saldo - valor;
+        }
+    }
+    public void deposito(double valor){
+        this.saldo = saldo + valor;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+    public void transfer(double valor, Conta conta2) {
+        this.saque(valor);
+        conta2.setSaldo(conta2.getSaldo() + valor);
+    }
+}
