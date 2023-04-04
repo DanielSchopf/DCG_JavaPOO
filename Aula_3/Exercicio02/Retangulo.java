0bf7d88d3f828022012f80fcbca3d7e01af6fed7
Daniel Schopf <74994656+DanielSchopf@users.noreply.github.com>
2023-04-04 20:59:45 -0300

    Create Retangulo.java

---
+++ Aula_3/Exercicio02/Retangulo.java
@@ -0,0 +1,26 @@
+package Aula3;
+
+public class Retangulo {
+    public double altura, largura;
+    public double getLargura() {
+        return largura;
+    }
+    public void setLargura(double largura) {
+        this.largura = largura;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double calcular_area(double x, double y){
+        return x*y;
+    }
+    public double calcular_perimetro(double x, double y){
+        return 2*(x+y);
+    }
+}
